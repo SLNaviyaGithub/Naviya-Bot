8455b30c4208fdda7ad1557efcbc5ace9304f368
SLNaviyaGithub <86926146+SLNaviyaGithub@users.noreply.github.com>
2024-12-27 22:54:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "X7g0EDKC#K8lJVIyObVSVe0VnWPvmgw0a8tzPvcIvy-ZAUdGNdZ0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
